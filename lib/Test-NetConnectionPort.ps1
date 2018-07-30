8b45a8184794457017ea3ba4b2b09a21c43e3b09
itscterrytin <itscterrytin@users.noreply.github.com>
2018-07-30 16:44:06 +0800

    Add files via upload

---
+++ lib/Test-NetConnectionPort.ps1
@@ -0,0 +1,21 @@
+﻿function Test-NetConnectionPort {
+ Param ($TargetHost, $Port, $ThresholdSecond)
+ $testMeaure = measure-command {
+#  $testResult = Test-NetConnection $TargetHost -Port $Port
+  $testResult = New-Object System.Net.Sockets.TCPClient -ArgumentList $TargetHost, $Port
+ }
+# $testResult
+
+# if ( $($testResult).TcpTestSucceeded ) {
+ if ( $($testResult).Connected ) {
+  return 0 # port allowed
+ } elseif ( $($testMeaure).TotalMilliseconds -ge $ThresholdSecond*1000 ) {
+  return 1 # port timeout
+ } else {
+  return 2 # port refused
+ }
+}
+
+Test-NetConnectionPort -TargetHost 'localhost' -Port 445 -ThresholdSecond 5
+Test-NetConnectionPort -TargetHost 'localhost' -Port 22 -ThresholdSecond 5
+Test-NetConnectionPort -TargetHost '111.111.111.111' -Port 22 -ThresholdSecond 5
